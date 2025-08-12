a5932ce7581c12eb499502da79b809e570371df3
Saravenpi <saravenpi@tuta.io>
2025-08-12 22:06:04 +0200

    🐛 Fixed

--- apps/knot-space-ui/src/routes/settings/+layout.svelte
+++ apps/knot-space-ui/src/routes/settings/+layout.svelte
@@ -4,6 +4,7 @@
   import { authStore } from '../../lib/stores';
 
   onMount(() => {
+    authStore.initialize();
     const unsubscribe = authStore.subscribe(({ initialized, isAuthenticated }) => {
       if (initialized && !isAuthenticated) {
         goto('/login?redirectTo=/settings');
